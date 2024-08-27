a9a849a69d776dc612e13bdb3135600000a98120
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-08-27 12:21:57 +0000

    proyecto Generador de Excusa

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Mi perro ", "Mi abuela ", "El cartero ", "Mi pajarito "];
+  let action = ["comió ", "orinó ", "aplastó ", "rompió "];
+  let what = ["mi tarea ", "mi celular ", "el auto "];
+  let when = [
+    "antes de ir a clases",
+    "cuando estaba durmiendo",
+    "mientras estaba haciendo ejercicio",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+
+  function generateRandomExcuse() {
+    const getRandomIndex = arr => Math.floor(Math.random() * arr.length);
+
+    const quien = getRandomIndex(who);
+    const que = getRandomIndex(what);
+    const hizo = getRandomIndex(action);
+    const cuando = getRandomIndex(when);
+
+    const excuse = `${who[quien]} ${action[hizo]} ${what[que]} ${when[cuando]}`;
+
+    document.querySelector("#excuse").innerHTML = excuse;
+  }
+
+  document
+    .querySelector("#generate")
+    .addEventListener("click", generateRandomExcuse);
 };
